d392cadcfc637f265e220aa201474fe7820ebc70
”AkselRoughsedge” <roughsedge.a@gmail.com>
2024-11-02 08:42:08 +0900

    Login and Signup component ready for connection

--- my-app/app/pages/home.tsx
+++ my-app/app/pages/home.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <nav>
         <Navigation></Navigation>
       </nav>
-      <main className="bg-gray-300 dark:bg-gray-700 w-full h-screen flex justify-center">
+      <main className="bg-gray-200 dark:bg-gray-700 w-full h-screen flex justify-center">
         <div className="bg-slate-400 dark:bg-slate-600 h-full w-4/5">
 
         </div>
--- my-app/app/pages/landingpage.tsx
+++ my-app/app/pages/landingpage.tsx
@@ -2,60 +2,54 @@
 import { useState } from "react";
 
 export default function LandingPage() {
-const [SignupState, setSignupState] = useState("Login")
+const [isLogin, setIsLogin] = useState(true)
 
 //Toggle viewing signup or login
 const toggleView = () => {
-    if (SignupState == "Signup") {
-        setSignupState("Login")
-    } else if (SignupState == "Login") {
-        setSignupState("Signup")
+    if (isLogin) {
+        setIsLogin(false)
+    } else {
+        setIsLogin(true)
     }
 }
 
-//Login and Signup components
-const Signup = () => {
-    if (SignupState === "Signup") {
+const login = () => {
+
+}
+
+const signup = () => {
+
+}
+
+//Login and Signup component
+const LoginSignup = () => {
         return (
             <div className="w-full h-full">
-                <div className="dark:bg-slate-800 bg-slate-300 rounded-md flex flex-col p-12 space-y-4">
-                    <h3 className="text-xl">Signup</h3>
-                    <input className="rounded-md dark:bg-slate-600 p-1" placeholder="Username" type="text"></input>
+                <div className="dark:bg-slate-800 bg-gray-200 rounded-xl flex flex-col p-12 space-y-4">
+                    <h3 className="text-xl">{isLogin ? "Login" : "Signup"}</h3>
+                    <input className="rounded-md dark:bg-slate-600 p-1" placeholder="email" type="email"></input>
                     <input className="rounded-md dark:bg-slate-600 p-1"  placeholder="password" type="password"></input>
-                    <button onClick={toggleView} className="w-min">Login</button>
+                    <input className={ "rounded-md dark:bg-slate-600 p-1 " + (isLogin ? "hidden" : "") }  placeholder="repeat password" type="password"></input>
+                    <button className="w-full bg-blue-200 dark:bg-slate-500 rounded-md p-1 !mt-8 hover:bg-blue-100 dark:hover:bg-slate-400">{isLogin ? "Login" : "Create account"}</button>
+                    <button onClick={toggleView} className="w-fit p-1 rounded-md">{isLogin ? "Create account" : "Login"}</button>
                 </div>
             </div>
         )
-    } else if (SignupState === "Login") {
-        return (
-            <div className="w-full h-full">
-                <div className="dark:bg-slate-800 bg-slate-300 rounded-md flex flex-col p-12 space-y-4">
-                    <h3 className="text-xl">Login</h3>
-                    <input className="rounded-md dark:bg-slate-600 p-1" placeholder="Username" type="text"></input>
-                    <input className="rounded-md dark:bg-slate-600 p-1"  placeholder="password" type="password"></input>
-                    <button onClick={toggleView} className="w-min">Create account</button>
-                </div>
-            </div>
-        )
-    }
 }
 
   return (
     <div className="">
-      <main className="bg-gray-200 dark:bg-gray-700 w-full h-screen flex justify-center">
+      <main className="bg-gray-100 dark:bg-gray-700 w-full h-screen flex justify-center">
         <div className=" h-full w-4/5 flex justify-center items-center">
             <div className="h-2/5 flex w-full">
                 <div className="w-full h-full">
                     <h1 className="text-7xl">Welcome.</h1>
                     <h2 className="text-xl">Please login or create an account.</h2>
                 </div>
-                <Signup></Signup>
+                <LoginSignup></LoginSignup>
             </div>
         </div>
       </main>
-      <footer className="">
-        
-      </footer>
     </div>
   );
 }
